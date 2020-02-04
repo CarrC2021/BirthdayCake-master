0e08f482cad1a8ae4fe9929f8de7cf36a522c851
CarrC2021 <57923118+CarrC2021@users.noreply.github.com>
2020-02-03 21:21:25 -0800

    Finally done with the listeners

--- app/src/main/java/cs301/birthdaycake/CakeController.java
+++ app/src/main/java/cs301/birthdaycake/CakeController.java
@@ -2,7 +2,12 @@ package cs301.birthdaycake;
 
 //this class will be controlling the operations on the cak and will determine what will appear in the cakeView
 
-public class CakeController {
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+
+public class CakeController implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener{
 
     private CakeView cakeView;
     private CakeModel cakeModel;
@@ -12,6 +17,37 @@ public class CakeController {
         cakeModel = cakeView.getCakeModel();
     }
 
+    public void onClick(CakeView view){
+        android.util.Log.d("CakeController", "been clicked");
+    }
 
+    public void onClick(View v) {
+        Log.d("Blow Out", "button got clicked");
+        cakeModel.candlesLit = false;
+        cakeView.invalidate();
+    }
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Log.d("Has Candles", "onCheckedChanged");
+        cakeModel.hasCandles = isChecked;
+        cakeView.invalidate();
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.d("seekbar", " new numCandles");
+        cakeModel.numCandles = progress;
+        cakeView.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
 }
--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
         CakeView cakeView = findViewById(R.id.cakeview);
-        new CakeController(cakeView);
+        CakeController controller = new CakeController(cakeView);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(controller);
+
+        CompoundButton compound = findViewById(R.id.switch2);
+        compound.setOnCheckedChangeListener(controller);
+
+        SeekBar bar = findViewById(R.id.seekBar);
+        bar.setOnSeekBarChangeListener(controller);
     }
 
     public void goodbye(View button) {
         Log.i("button", "Goobye");
     }
+
+
 }
